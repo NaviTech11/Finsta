74aad0e4005dde7958879ffc052554a9a7c41f93
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-31 14:33:54 -0500

    Add, Card, Text, Screen Components

---
+++ app/screens/PostEditScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function PostEdit(props) {
+  return (
+    <View style={styles.container}>{'PostEditScreen'}</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default PostEdit;
